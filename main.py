0d6f2a00fc700d270f279db184b269ede45b3ab0
Timon <74593747+T1moooon@users.noreply.github.com>
2024-11-04 04:21:55 +0300

    Edits

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def wait(author_id, message):
+def wait(author_id, message, bot):
     delay = pytimeparse.parse(message)
     if delay is not None and delay > 0:
         message_id = bot.send_message(author_id, "Запускаю таймер...")
-        bot.create_countdown(delay, notify_progress, message_id=message_id, author_id=author_id, total=delay)
+        bot.create_countdown(delay, notify_progress, message_id=message_id, author_id=author_id, total=delay, bot=bot)
     else:
         bot.send_message(author_id, "Введите корректное время, например, '5s', '2m', '1h'.")
 
 
-def notify_progress(secs_left, message_id, author_id, total):
+def notify_progress(secs_left, message_id, author_id, total, bot):
     progress_bar = render_progressbar(total, total - secs_left, length=30, fill='█', zfill='░')
     if secs_left > 0:
         bot.update_message(author_id, message_id, f"Осталось {secs_left} секунд\n{progress_bar}")
@@ -32,11 +32,10 @@ def notify_progress(secs_left, message_id, author_id, total):
 
 
 def main():
-    global bot
     load_dotenv()
     tg_token = os.getenv("TG_TOKEN")
     bot = ptbot.Bot(tg_token)
-    bot.reply_on_message(wait)
+    bot.reply_on_message(wait, bot=bot)
     bot.run_bot()
 
 
